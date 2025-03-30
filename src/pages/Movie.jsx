9107579d80878c11758e8970d68295922a5399a2
AvinashS97 <avikumar97170@gmail.com>
2025-03-30 21:34:11 +0530

    Axios_FullStack

---
+++ src/pages/Movie.jsx
@@ -0,0 +1,23 @@
+import axios from "axios";
+import { useEffect } from "react";
+
+export const Movie = () => {
+
+    const API = "https://www.omdbapi.com/?i=tt3896198&apikey=d267e12e&s=titanic&page=1";
+
+    const getMovieData = async() => {
+        try {
+            const res = await axios.get(API)
+            console.log(res.data.Search);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    useEffect(()=> {
+        getMovieData();
+    },[])
+
+    return <h1> Movie Page</h1>
+
+}
