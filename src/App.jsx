45b2052dbaea8601371dacc41a9fa42066304f91
AvinashS97 <avikumar97170@gmail.com>
2025-04-01 17:02:50 +0530

    V90: APIs: Mastering CRUD

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
-import { Movie } from "./pages/Movie";
+// import { Movie } from "./pages/Movie";
+import { useEffect } from "react";
+import { getPost } from "./api/Postapi";
+
 
 const App = () => {
-  return <Movie />
+  const getPostData = async() => {
+    const res = await getPost();
+    console.log(res);
+  }
+
+  useEffect(()=> {
+    getPostData();
+  })
+
+  return <h1>  Hello React CRUD Operation </h1>
 };
 
 export default App;
