da8aebae3bbe16af4ebe475e59a39a01fe10d940
AvinashS97 <avikumar97170@gmail.com>
2025-04-03 12:42:21 +0530

    v92 dONE

--- src/api/PostApi.jsx
+++ src/api/PostApi.jsx
@@ -1,26 +1,26 @@
-import axios from "axios";
+import axios from 'axios'
 
 const api = axios.create({
-    baseURL: "https://jsonplaceholder.typicode.com",
-});
+  baseURL: 'https://jsonplaceholder.typicode.com'
+})
 
 // Get Method
 
 export const getPost = () => {
-    return api.get("/posts");
+  return api.get('/posts')
 }
 
-// delete method
-export const deletePost = (id) => {
-    return api.delete(`/posts/${id}`);
-  };
-  
-  //post method
-  export const postData = (post) => {
-    return api.post("/posts", post);
-  };
-  
-  //put method
-  export const updateData = (id, post) => {
-    return api.put(`/posts/${id}`, post);
-  };
+// Delete Method
+export const deletePost = id => {
+  return api.delete(`/posts/${id}`)
+}
+
+//Post Method
+export const postData = post => {
+  return api.post('/posts', post)
+}
+
+// Put Method
+export const updateData = (id, post) => {
+  return api.put(`/posts/${id}`, post)
+}
--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,65 +1,74 @@
-import { useEffect, useState } from "react";
-import { getPost } from "../api/Postapi";
-import "../App.css";
-import { Form } from "./Form";
+import { useEffect, useState } from 'react'
+import { deletePost, getPost } from '../api/Postapi'
+import '../App.css'
+import { Form } from './Form'
 
 export const Posts = () => {
+  const [data, setData] = useState([])
+  const [updateDataApi, setUpdateDataApi] = useState({})
 
-    const [data , setData] = useState([]);
-    const [updateDataApi, setUpdateDataApi] = useState({});
+  const getPostData = async () => {
+    const res = await getPost()
+    console.log(res.data)
+    setData(res.data)
+  }
 
-    const getPostData = async () => {
-        const res = await getPost();
-        console.log(res.data);
-        setData(res.data);
-    };
+  useEffect(() => {
+    getPostData()
+  }, [])
 
-    useEffect(()=> {
-        getPostData();
-    }, []);
+  // Function to Delete Post
 
-    // Function to Delete Post
-
-    const handleDeletePost = async(id) => {
-        try {
-            const res= await handleDeletePost
-        } catch (error) {
-            console.log(error);
-        }
-
-    };
-
-    // Function to Edit Post
-
-    const handleUpdatePost = () => {
-        
+  const handleDeletePost = async id => {
+    try {
+      const res = await deletePost(id)
+      if (res.status === 200) {
+        const newUpdatedPosts = data.filter(curPost => {
+          return curPost.id !== id
+        })
+        setData(newUpdatedPosts)
+      } else {
+        console.log('Failed to delete the post:', res.status)
+      }
+    } catch (error) {
+      console.log(error)
     }
+  }
 
+  // Function to Edit Post
 
+  const handleUpdatePost = curElem => setUpdateDataApi(curElem)
 
-
-    return (
-        <>
-        <section className="section-form">
-            <Form data={data} setData={setData} updateDataApi={updateDataApi} setUpdateDataApi={setUpdateDataApi}/>
-        </section>
-        <section className="section-post">
-            <ol>
-                {
-                    data.map((curElem) => {
-                        const { id, body, title} = curElem;
-                        return (
-                            <li key={id}>
-                                <p>Title: {title}</p>
-                                <p>Body: {body}</p>
-                                <button  onClick={() => handleUpdatePost(curElem)}>Edit</button>
-                                <button className="btn-delete" onClick={() => handleDeletePost(id)}>Delete</button>
-                            </li>
-                        )
-                    })
-                }
-            </ol>
-        </section>
-        </>
-    ) 
+  return (
+    <>
+      <section className='section-form'>
+        <Form
+          data={data}
+          setData={setData}
+          updateDataApi={updateDataApi}
+          setUpdateDataApi={setUpdateDataApi}
+        />
+      </section>
+      <section className='section-post'>
+        <ol>
+          {data.map(curElem => {
+            const { id, body, title } = curElem
+            return (
+              <li key={id}>
+                <p>Title: {title}</p>
+                <p>Body: {body}</p>
+                <button onClick={() => handleUpdatePost(curElem)}>Edit</button>
+                <button
+                  className='btn-delete'
+                  onClick={() => handleDeletePost(id)}
+                >
+                  Delete
+                </button>
+              </li>
+            )
+          })}
+        </ol>
+      </section>
+    </>
+  )
 }
